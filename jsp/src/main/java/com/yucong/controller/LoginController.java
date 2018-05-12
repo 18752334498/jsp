c1534f2461c77168630878eb2861dbeaf224c82d
18752334498 <2424693824@qq.com>
2018-05-12 08:53:50 +0800

    jsp项目

---
+++ jsp/src/main/java/com/yucong/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.yucong.controller;
+
+import com.yucong.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login (ModelMap map) {
+
+        User user = new User(1,"jack",11);
+        map.put("user",user);
+
+        User person = new User(2,"rose",22);
+        map.addAttribute("person",person);
+
+        return "/login/index";
+    }
+    @RequestMapping("/other")
+    public String other (ModelMap map) {
+
+        User user = new User(654321,"other",123456);
+        map.put("user",user);
+
+        return "/other/index";
+    }
+
+}
---
+++ jsp/target/classes/META-INF/resources/controller/other/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" pageEncoding="UTF-8" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body style="text-align: center; font-size: 150%">
+    <c:if test="${empty user}">
+        <span style="color: blue">跳过了controller,直接访问了jsp...</span>
+    </c:if>
+    <p>"${user.id }"</p>
+    <p>"${user.name }"</p>
+    <p>"${user.age }"</p>
+    <br/>
+</body>
+</html>
